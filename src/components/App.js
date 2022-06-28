52d960dee5b826141d02a4a649303c36e4ea1dfb
LonlehKitteh <krzysztof02gach@gmail.com>
2022-06-28 15:19:36 +0200

    react router dom

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import Navigation from "./Navigation"
+import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Service from "./pages/Service"
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/service" element={<Service />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,24 @@
+import { Link, useMatch, useResolvedPath } from 'react-router-dom'
+
+export default function Navigation() {
+    return (
+        <div className='nav'>
+            <Link to="/" className='site-title'>React Basics</Link>
+            <ul>
+                <CustomLink to='/about'>About</CustomLink>
+                <CustomLink to='/service'>Service</CustomLink>
+            </ul>
+        </div>
+    )
+}
+
+function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>{children}</Link>
+        </li>
+    )
+}
