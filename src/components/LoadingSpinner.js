57c77a3c29bec40fead7d43fa52f9d9d40265f47
LonlehKitteh <krzysztof02gach@gmail.com>
2022-06-30 02:58:02 +0200

    spinner

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,7 +1,7 @@
-import React from 'react'
-
 export default function LoadingSpinner() {
     return (
-        <div>LoadingSpinner</div>
+        <div className="spinner">
+            <span className="loader"></span>
+        </div>
     )
 }
