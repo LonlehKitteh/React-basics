cfaba1a3fe8c1ef843154b068a495818d1ef3cf0
Firecry <lubiexd80@gmail.com>
2022-07-04 01:54:49 +0200

    some navbar updates

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,7 @@ import HomeImage from '../../assets/images/home.webP'
 export default function Home() {
     return (
         <React.Fragment>
-            <Image alt='abstraction' className="position-absolute top-0 w-100 h-100" fluid src={HomeImage} />
+            <Image alt='abstraction' className="position-relative top-0 w-100 h-50" fluid src={HomeImage} />
             <div>Home</div>
         </React.Fragment>
     )
