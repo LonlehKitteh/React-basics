3d434780fe0785aca0bd38b1f95d4d3bfa640a09
LonlehKitteh <krzysztof02gach@gmail.com>
2022-07-06 22:45:12 +0200

    repaired search button

---
+++ src/components/layout/components/Search.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from 'react-bootstrap/Form';
+
+const Search = () => {
+    return (
+        <div className='search'>
+            <div className='search-title'></div>
+            <Form className="d-flex">
+                <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
+            </Form>
+        </div>
+    )
+}
+
+export default Search
