4801bffa11cef90317e492a8d1534cb86a5d9220
LonlehKitteh <krzysztof02gach@gmail.com>
2022-07-07 02:56:49 +0200

    font awesome arrived

--- src/components/layout/components/Search.js
+++ src/components/layout/components/Search.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 const Search = () => {
     return (
         <div className='search'>
             <div className='search-title'></div>
-            <Form className="d-flex">
-                <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
+            <Form>
+                <Form.Group className="d-flex">
+                    <Form.Label htmlFor='icon' className='search-btn'>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} size="2x" className='search-btn' />
+                    </Form.Label>
+                    <Form.Control type="search" id="icon" placeholder="Search" className="me-2" aria-label="Search" />
+                </Form.Group>
             </Form>
         </div>
     )
