ed609a7e16742e3771ba8f7615c32dc2f9047a70
Firecry <lubiexd80@gmail.com>
2022-07-10 21:33:51 +0200

    huge icon change

--- src/components/layout/components/Search.js
+++ src/components/layout/components/Search.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 const Search = (props) => {
     return (
         <Form>
             <Form.Group className="d-flex">
+                {/* icon place */}
                 <Form.Control type="search" id="icon" placeholder="Search" className="me-2" aria-label="Search" />
-                <Form.Label htmlFor='icon' className='search-btn'>
-                    <FontAwesomeIcon icon={faMagnifyingGlass} size="2x" className='search-btn' inverse={!props.isScrolled} />
-                </Form.Label>
             </Form.Group>
         </Form>
     )
