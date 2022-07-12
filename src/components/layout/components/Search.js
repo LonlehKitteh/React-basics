2cbd143703cea38d47aa5d2555d97adddbf05fa0
Firecry <lubiexd80@gmail.com>
2022-07-12 16:58:34 +0200

    deleted props

--- src/components/layout/components/Search.js
+++ src/components/layout/components/Search.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form';
 
-const Search = (props) => {
+const Search = () => {
     return (
         <Form>
             <Form.Group className="d-flex">
